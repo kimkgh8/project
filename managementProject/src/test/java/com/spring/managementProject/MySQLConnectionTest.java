d48e7db5f0cc84144cbf74aaa402fd088555988c
KimGuenho <kimkgh8@gmail.com>
2021-03-31 21:06:32 +0900

    로거 사용

--- managementProject/src/test/java/com/spring/managementProject/MySQLConnectionTest.java
+++ managementProject/src/test/java/com/spring/managementProject/MySQLConnectionTest.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MySQLConnectionTest {
 	static final String DRIVER = "com.mysql.jdbc.Driver";
@@ -17,57 +19,61 @@ public class MySQLConnectionTest {
 	@Test
 	public void getMySQLConnectionTest() {
 
+		final Logger logger = LoggerFactory.getLogger(this.getClass());
+		
 		Connection conn = null;
-		Statement stmt = null;
-
-		try {
-
-			System.out.println("==================== MySQL Connection START ====================");
-
-			Class.forName(DRIVER);
-
-			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-			stmt = conn.createStatement();
-
-			String sql = "SELECT BOARD_SUBJECT, BOARD_CONTENT, BOARD_WRITER FROM TB_BOARD";
-
-			ResultSet rs = stmt.executeQuery(sql);
-			while (rs.next()) {
-
-				String boardSubject = rs.getString("BOARD_SUBJECT");
-				String boardContent = rs.getString("BOARD_CONTENT");
-				String boardWriter = rs.getString("BOARD_WRITER");
-
-				System.out.print("boardSubject : " + boardSubject + ", ");
-				System.out.print("boardContent: " + boardContent + ", ");
-				System.out.println("boardWriter: " + boardWriter);
-			}
-
-			rs.close();
-			stmt.close();
-			conn.close();
-
-		} catch (SQLException se1) {
-			se1.printStackTrace();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-			try {
-				if (stmt != null) {
-					stmt.close();
-				}
-			} catch (SQLException se) {
-				se.printStackTrace();
-			}
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException se) {
-				se.printStackTrace();
-			}
-		}
-
-		System.out.println("==================== MySQL Connection END ====================");
-	}
+        Statement stmt = null;
+        
+        try {
+            
+            logger.info("==================== MySQL Connection START ====================");
+            
+            Class.forName(DRIVER);
+            
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            stmt = conn.createStatement();
+ 
+            String sql = "SELECT BOARD_SUBJECT, BOARD_CONTENT, BOARD_WRITER FROM TB_BOARD";
+ 
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                
+                String boardSubject = rs.getString("BOARD_SUBJECT");
+                String boardContent = rs.getString("BOARD_CONTENT");
+                String boardWriter = rs.getString("BOARD_WRITER");
+ 
+                logger.info("boardSubject : {}", boardSubject);
+                logger.info("boardContent: {}", boardContent);
+                logger.info("boardWriter: {}", boardWriter);
+                logger.info("========================================");
+                
+            }
+ 
+            rs.close();
+            stmt.close();
+            conn.close();
+ 
+        } catch (SQLException se1) {
+            se1.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        
+        logger.info("==================== MySQL Connection END ====================");
+    }
 }
